cf85bea12ec60f7fe390832c1d10b42cdc35f324
Miguel Velazquez <VelazquezMM@hendrix.edu>
2023-03-13 20:05:41 -0500

    Added new scenes, SceneLoader script

---
+++ Last lab game/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
