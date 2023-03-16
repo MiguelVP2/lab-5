180a2052e09400107d080e1d3e09742343dade89
Miguel Velazquez <VelazquezMM@hendrix.edu>
2023-03-15 20:38:07 -0500

    added player script, still needs work, added new shaders, health bar

---
+++ Last lab game/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_bar : MonoBehaviour
+{
+    public int max_health = 100;
+    public int c_health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        c_health = max_health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        
+    }
+
+    void damage()
+    {
+
+    }
+}
