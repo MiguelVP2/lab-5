02f7bba70e3398e29afe467fa0d14935331fbc08
Miguel Velazquez <VelazquezMM@hendrix.edu>
2023-03-17 10:45:59 -0500

    added battle script, work in progress

---
+++ Last lab game/Assets/Scripts/Battles.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Bstate {START, PLAYERTURN, ENEMYTURN, WON, LOST}
+public class Battles : MonoBehaviour
+{
+    public Bstate state;
+    public GameObject player;
+    public GameObject enemy;
+    public Transform playerBattle;
+    public Transform enemyBattle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = Bstate.START;
+        SetBattle();
+    }
+
+    void SetBattle()
+    {
+        GameObject playerGO = Instantiate(player, playerBattle);
+        Instantiate(enemy, enemyBattle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
